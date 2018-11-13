7e5860d0cee8e489aff4edab05ecdf16028e4bec
setZ <ramiras.ukr@gmail.com>
2018-11-13 09:03:00 +0200

    fixed buggs

--- common.js
+++ common.js
@@ -7,28 +7,18 @@ var findW = function(){
 	var arTop = [];
 	var sum = 0;
 	var minTop = 0;
+
 	for(var i = 0; i < ar.length; i++){
-		var b1 = (ar[i] >= ar[i + 1] && (i + 1) <= ar.length);
-		var b2 =((i == ar.length - 1) && ar[i] >= ar[i - 1]);
-		var b3 = ((ar[i - 1] <= ar[i] && (i - 1) >= 0) || i == 0);
-		
-		if(	(b1 || b2) && b3 ){
+		if(	((ar[i] >= ar[i + 1] && (i + 1) <= ar.length) || ((i === ar.length - 1) && ar[i] >= ar[i - 1])) && ((ar[i - 1] <= ar[i] && (i - 1) >= 0) || i === 0) ) {
 			arTop.push(i);
 		}
-		
-		var h = 0;
-		while(true)
-		{
-			if(h++ > ar.length)
-				break;
-			if(ar[i + h] >= ar[i])
-			{
+		for(var h = 1; h < ar.length; h++) {
+			if(ar[i + h] >= ar[i]) {
 				i = i + h - 1;
-				break;
+			break;
 			}
 		}
-	 }
-	
+	}	
 	for (var i = 0; i < arTop.length - 1; i++){
 		if(ar[arTop[i]] < ar[arTop[i + 1]]){
 			minTop = arTop[i];
@@ -40,7 +30,6 @@ var findW = function(){
 		}
 	}
 	console.log("The number of elements filled with woter: " + sum);
-
 }
 
 findW(ar)
